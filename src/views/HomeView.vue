3e8aa0f02db14fc850b959157a78318d197c882e
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-11-03 16:23:06 +0700

    refactor: remove unused services

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container mw-[1024px] mx-auto">
-    <div class="text-center text-4xl">Welcome to Pokemon workshop !</div>
+    <div class="text-center text-4xl">Welcome to Shop project !</div>
   </div>
 </template>
